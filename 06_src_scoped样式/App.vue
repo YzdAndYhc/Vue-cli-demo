d02c827a6178b9cf70abceaa6e37396cccf6c012
yzd <3053707776@qq.com>
2022-07-29 16:56:52 +0800

    vue-cli

---
+++ 06_src_scoped样式/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <Student/>
+    <hr>
+    <School/>
+  </div>
+</template>
+
+<script>
+    //引入Shool组件
+    import Student from './components/Student'
+    import School from './components/School'
+
+    export default {
+        name:'App',
+        components: { Student,School },
+    }
+</script>
+
+<style>
+
+</style>
