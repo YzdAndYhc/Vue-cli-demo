b467da5920a9a88eb5819bb79e556b547cd593ef
yzd <3053707776@qq.com>
2022-08-05 16:02:55 +0800

    插槽slot

---
+++ 22_插槽/1_src_默认插槽/components/Category.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="category">
+        <h3>{{title}}</h3>
+        <slot></slot>
+    </div>
+</template>
+
+<script>
+    export default {
+        name:'Category',
+        props:['title']
+    }
+</script>
+
+<style lang="css" scoped>
+    .category{
+        background-color: aliceblue;
+        width: 200px;
+        height: 300px;
+    }
+    h3{
+        text-align: center;
+        background-color: aqua;
+    }
+</style>
---
+++ 22_插槽/2_src_具名插槽/App.vue
@@ -0,0 +1,62 @@
+<template>
+    <div id="app">
+        <div class="container">
+            <Category title="食物">
+                <img slot="center" src="https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fimg.jj20.com%2Fup%2Fallimg%2Ftp05%2F1Z9291TIBZ6-0-lp.jpg&refer=http%3A%2F%2Fimg.jj20.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=auto?sec=1662273471&t=9b08b21a2c022b951fb00733b38a8e4b" />
+                <div class="foot" slot="footer">
+                    <a href="https://www.baidu.com">查看更过多...</a>
+                </div>
+            </Category>
+            <Category title="游戏" :games="games">
+                <ul slot="center">
+                    <li v-for="(item,index) in games" :key="index">
+                        {{item}}
+                    </li>
+                </ul>
+                <div class="foot" slot="footer">
+                    <a href="https://www.baidu.com">单机游戏</a>
+                    <a href="https://www.baidu.com">网络游戏</a>
+                </div>
+            </Category>
+            <Category title="电影">
+                <video controls src="http://video.sina.cn/ent/other/2019-09-23/detail-iicezzrq7819572.d.html?vt=4&wm=30490006996980508"></video>
+                <template v-slot:footer>
+                    <div class="foot" slot="footer">
+                        <a href="https://www.baidu.com">热门</a>
+                        <a href="https://www.baidu.com">推荐</a>
+                        <a href="https://www.baidu.com">上映</a>
+                    </div>
+                    <h4>欢迎前来！</h4>
+                </template>
+            </Category>
+        </div>
+    </div>
+</template>
+
+<script>
+import Category from './components/Category'
+    export default {
+        name:'App',
+        components: {Category},
+        data() {
+            return {
+                foods:['烧烤','牛排','小龙虾','炒饭'],
+                games:['CF','CSGO','LOL','ESAS'],
+                movies:['[战狼1]','[战狼2]','[战狼3]','[战狼4]'],
+            }
+        },
+    }
+</script>
+
+<style scoped>
+    .container,.foot{
+        display: flex;
+        justify-content: space-around;
+    }
+    video,img{
+        width: 100%;
+    }
+    h4{
+        text-align: center;
+    }
+</style>
